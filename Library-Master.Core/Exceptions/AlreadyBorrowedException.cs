01b5a00d96786565f0ca376d9a608277ce3581a4
Michael <michaelgmx1@googlemail.com>
2021-02-17 13:49:59 +0100

    Added borrow function for book

---
+++ Library-Master.Core/Exceptions/AlreadyBorrowedException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Library_Master.Core.Exceptions
+{
+    public class AlreadyBorrowedException : Exception
+    {
+
+        public AlreadyBorrowedException()
+        {
+        }
+
+        public AlreadyBorrowedException(string message) : base(message)
+        {
+        }
+
+        public AlreadyBorrowedException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
---
+++ Library-Master.Core/Services/BorrowService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Library_Master.Core.Exceptions;
+using Library_Master.Core.Models;
+using Library_Master.Core.Services.TransactionServices;
+
+namespace Library_Master.Core.Services
+{
+    public class BorrowService : IBorrowService
+    {
+        private readonly IDataService<Account> _accountService;
+        private readonly IDataService<Book> _bookService;
+
+        public BorrowService(IDataService<Account> accountService, IDataService<Book> bookService)
+        {
+            _accountService = accountService;
+            _bookService = bookService;
+        }
+
+        public async Task<Account> BorrowBook(Account lender, Book book)
+        {
+            var borrowBook = await _bookService.Get(book.Id);
+            var lenderAcc = await _accountService.Get(lender.Id);
+
+            if (borrowBook != null || lenderAcc != null)
+            {
+                if (borrowBook.Entliehen != true)
+                {
+                    lender.Transaktionen.Add(new Transaktion()
+                    {
+                        EntliehenVon = lender,
+                        EntliehenAm = DateTime.Now,
+                        Book = book
+                    });
+                    book.Entliehen = true;
+                }
+                else
+                {
+                    throw new AlreadyBorrowedException();
+                }
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+
+            await _bookService.Update(book.Id, book);
+            await _accountService.Update(lender.Id, lender);
+
+            return lender;
+        }
+    }
+}
